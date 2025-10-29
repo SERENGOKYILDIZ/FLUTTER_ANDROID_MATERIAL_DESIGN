927d04e78e8d9c7f601b32943f1f50ba3c25dc22
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-10-29 19:03:35 +0300

    Learned search to appbar

--- lib/main.dart
+++ lib/main.dart
@@ -26,67 +26,43 @@ class Anasayfa extends StatefulWidget {
 }
 
 class _AnasayfaState extends State<Anasayfa> {
+
+  bool aramaDurumu = false;
+
   @override
   Widget build(BuildContext context) {
-
-    var formKey = GlobalKey<FormState>();
-    var tfUsername = TextEditingController();
-    var tfPassword= TextEditingController();
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
         foregroundColor: Colors.white,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Başlık", style: TextStyle(color: Colors.white, fontSize: 16)),
-            Text("Alt Başlık", style: TextStyle(color: Colors.white, fontSize: 12)),
-          ],
-        ),
-        // centerTitle: true, ///-> Başlık ortalanır.
-        leading: IconButton( ///-> leading sol taraf oluyor.
-          tooltip: "Menu Icon",
-          icon: Icon(Icons.dehaze),
-          onPressed: (){
-            print("Menu ikon tıklandı!");
+        title: aramaDurumu ?
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Arama için bir şey yazın..."
+          ),
+          onChanged: (aramaSonucu){
+            print("Arama sonucu $aramaSonucu");
           },
-        ),
-        actions: [ ///-> actions sağ taraf oluyor.
-          TextButton(
-            child: Text("ÇIKIŞ", style: TextStyle(color: Colors.white)),
-            onPressed: (){
-              print("ÇIKIŞ tıklandı!");
-            },
-          ),
+        ) : Text("AppBar Arama"),
+        actions: [
+          aramaDurumu ?
           IconButton(
-            tooltip: "Bilgi",
-            icon: Icon(Icons.info_outline),
+            icon: Icon(Icons.cancel),
             onPressed: (){
-              print("Bilgi ikon tıklandı!");
-            },
-          ),
-          PopupMenuButton(
-            child: Icon(Icons.more_vert),
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                value: 1,
-                child: Text("Sil"),
-              ),
-              PopupMenuItem(
-                value: 2,
-                child: Text("Güncelle"),
-              ),
-            ],
-            onSelected: (index){
-              if(index == 1){
-                print("Popup'da Sil seçildi!");
-              }
-              if(index == 2){
-                print("Popup'da Güncelle seçildi!");
-              }
+              setState(() {
+                aramaDurumu = false;
+              });
             },
           )
+          :
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              setState(() {
+                aramaDurumu = true;
+              });
+            },
+          ),
         ],
       ),
       body: Center(
