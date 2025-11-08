bcb9b7701ae574d0080bdbebbdb671a36e3366cc
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-11-08 16:32:59 +0300

    Learned using of ListView.builder, horizontal scroll

--- lib/main.dart
+++ lib/main.dart
@@ -38,41 +38,28 @@ class _AnasayfaState extends State<Anasayfa> {
         foregroundColor: Colors.white,
         title: Text("Material Design")
       ),
-      body: ListView.builder(
-        itemCount: ulkeler.length,
-        itemBuilder: (context, indeks){
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 50,
-                child: Row(
-                  children: [
-                    Text(ulkeler[indeks]),
-                    Spacer(),
-                    PopupMenuButton(
-                      child: Icon(Icons.more_vert),
-                      itemBuilder: (context) => [
-                        PopupMenuItem(value: 1, child: Text("Sil")),
-                        PopupMenuItem(value: 2, child: Text("Güncelle")),
-                      ],
-                      onSelected: (menuItemValue){
-                        if(menuItemValue == 1)
-                        {
-                          print("${ulkeler[indeks]} ülkesi silindi!");
-                        }
-                        if(menuItemValue == 2)
-                        {
-                          print("${ulkeler[indeks]} ülkesi güncellendi!");
-                        }
-                      },
-                    )
-                  ],
+      body: SizedBox(
+        height: 100,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal, ///-> Sıralama yönünü değiştirdik
+          itemCount: ulkeler.length,
+          itemBuilder: (context, indeks){
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(ulkeler[indeks]),
+                    ],
+                  )
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       )
     );
   }
